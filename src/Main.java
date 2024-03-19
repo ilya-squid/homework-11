bfcc96df302dea50e87d49251597dce63ff1c4bd
squid <sqids9s0@gmail.com>
2024-03-19 23:02:18 +0300

    homework 11

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //task 1
-        System.out.println("Задача 1");
+
+        isLeapYear(2000);
+        isLeapYear(2100);
+        isLeapYear(2024);
+
+        checkOS(0, 2000);
+        checkOS(1, 2024);
+
+        System.out.println("Потребуется дней для доставки: " + calculateTime(60));
     }
-}
+
+    public static void isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println("год является високосным");
+        } else {
+            System.out.println("год не является високосным");
+        }
+    }
+
+    public static void checkOS(int clientOS, int clientDeviceYear) {
+        int currtntYear = LocalDate.now().getYear();
+
+        if (clientOS == 0 && clientDeviceYear < currtntYear) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < currtntYear) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= currtntYear) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < currtntYear) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
+    public static int calculateTime(int distance){
+            if (distance <= 20) {
+                return 1;
+            } else if (distance > 20 && distance <= 60) {
+                return 2;
+            } else if (distance > 60 && distance <= 100) {
+                return 3;
+            }
+                return -1;
+            }
+    }
